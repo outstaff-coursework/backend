1ff16025b23a639a91d71da05cb54c6db6e737b6
BouFFaNTCRiB <bouf12w@gmail.com>
2023-12-15 22:09:01 +0300

    lint_jopa

--- app/services/staff/base_db_engine.py
+++ app/services/staff/base_db_engine.py
@@ -9,14 +9,14 @@ DATABASE_URL = "postgresql+asyncpg://postgres@localhost:5432/staff"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 Base = declarative_base()
-async_session = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
-)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
 
 async def init_models():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
 
 async def get_session() -> AsyncSession:
     async with async_session() as session:
--- app/services/staff/server.py
+++ app/services/staff/server.py
@@ -10,13 +10,16 @@ from base_db_engine import get_session, init_models
 
 app = FastAPI()
 
+
 class UserSearchSchema(BaseModel):
     name: str
     user_id: int
     position: str
 
+
 class UserSearchList(BaseModel):
     items: List[UserSearchSchema]
+
 
 class UserInfoSchema(BaseModel):
     name: str
@@ -27,21 +30,35 @@ class UserInfoSchema(BaseModel):
     user_about: str
     position: str
 
+
 @app.get("/user/{user_id}", response_model=UserInfoSchema)
 async def get_user(user_id: int, session: AsyncSession = Depends(get_session)):
     user = await service.get_user(user_id, session)
-    return UserInfoSchema(name=user.first_name + " " + user.last_name + " " + user.patronymic, nickname=user.nickname,
-                          email=user.email, telegram=user.telegram, phone_number=user.phone_number, user_about=user.user_about,
-                          position=user.position)
+    return UserInfoSchema(
+        name=user.first_name + " " + user.last_name + " " + user.patronymic,
+        nickname=user.nickname,
+        email=user.email,
+        telegram=user.telegram,
+        phone_number=user.phone_number,
+        user_about=user.user_about,
+        position=user.position,
+    )
+
 
 @app.get("/users/{request}", response_model=UserSearchList)
 async def search_users(request: str, session: AsyncSession = Depends(get_session)):
     users = await service.get_users(request, session)
-    items = [UserSearchSchema(name=user.first_name + " " + user.last_name + " " + user.patronymic,
-                              user_id=user.user_id, position=user.position) for user in users]
+    items = [
+        UserSearchSchema(
+            name=user.first_name + " " + user.last_name + " " + user.patronymic,
+            user_id=user.user_id,
+            position=user.position,
+        )
+        for user in users
+    ]
     return UserSearchList(items=items)
+
 
 if __name__ == "__main__":
     init_models()
     uvicorn_run(app, host="0.0.0.0", port=5555)
-        
