53864d8c5bb615e8745f71aa684c5ce8c5c40683
BouFFaNTCRiB <bouf12w@gmail.com>
2023-12-15 22:35:36 +0300

    last_fs

--- app/services/staff/base_db_engine.py
+++ app/services/staff/base_db_engine.py
@@ -1,5 +1,4 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
